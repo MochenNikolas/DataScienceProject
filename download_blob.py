b8657990978c6c4f3d126ba6edbe0e5d8762047a
Mochen <745698140@qq.com>
2021-09-13 18:33:32 +0800

    update knn & google cloud funtion code

---
+++ download_blob.py
@@ -0,0 +1,15 @@
+from google.cloud import storage
+#if need use it in google cloud function, shoule add 'google-cloud-storage==1.16.1' in requirements.txt
+
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+
+    blob.download_to_filename(destination_file_name)
+
+    print('Blob {} downloaded to {}.'.format(
+        source_blob_name,
+        destination_file_name))
