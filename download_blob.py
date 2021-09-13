c3e11995a0691748e7d81b8c3df93c885dbabd15
Mochen <745698140@qq.com>
2021-09-13 18:34:49 +0800

    commit the code use to download model.pickle from GC storage

--- download_blob.py
+++ download_blob.py
@@ -2,6 +2,7 @@ from google.cloud import storage
 #if need use it in google cloud function, shoule add 'google-cloud-storage==1.16.1' in requirements.txt
 
 
+
 def download_blob(bucket_name, source_blob_name, destination_file_name):
     """Downloads a blob from the bucket."""
     storage_client = storage.Client()
@@ -13,3 +14,4 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
     print('Blob {} downloaded to {}.'.format(
         source_blob_name,
         destination_file_name))
+
