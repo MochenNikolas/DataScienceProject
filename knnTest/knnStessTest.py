fc440f47b985820267a0741f8b25857bcaa959f4
Mochen <745698140@qq.com>
2021-09-16 11:53:48 +0800

    modify the cloud function entrance url

--- knnTest/knnStessTest.py
+++ knnTest/knnStessTest.py
@@ -4,7 +4,7 @@ import time
 import threading
 import random
 
-url = 'https://us-central1-alpine-province-325707.cloudfunctions.net/function-2'
+url = 'https://us-central1-alpine-province-325707.cloudfunctions.net/knn'
 temp_data = [8.2210e-02,2.2000e+01,5.8600e+00,0.0000e+00,4.3100e-01,6.9570e+00,6.8000e+00,8.9067e+00,7.0000e+00,3.3000e+02,1.9100e+01,3.8609e+02,3.5300e+00]
 raw_data = {'data':temp_data}
 data = json.dumps(raw_data)
@@ -53,7 +53,7 @@ class stressTest(object):
             t.join()
         t2 = time.time()
 
-        print("================stress test result")
+        print("================stress test result===============")
         print("URL: ", self.press_url)
         print("workNum: ", THREAD_NUM, "*", ONE_WORKER_NUM, "=", THREAD_NUM * ONE_WORKER_NUM)
         print("total time cost: ", t2 - t1)
@@ -66,10 +66,10 @@ class stressTest(object):
 
 
 if __name__ == '__main__':
-    test_url = 'https://us-central1-alpine-province-325707.cloudfunctions.net/function-2'
+    test_url = 'https://us-central1-alpine-province-325707.cloudfunctions.net/knn'
 
-    THREAD_NUM = 50 # the num of pthreads
-    ONE_WORKER_NUM = 5 #the loop times of each pthread
+    THREAD_NUM = 1 # the num of pthreads
+    ONE_WORKER_NUM = 1 #the loop times of each pthread
     LOOP_SLEEP = 0.1 #the gap between two request(s)
     ERROR_NUM = 0 #the num of error
 
